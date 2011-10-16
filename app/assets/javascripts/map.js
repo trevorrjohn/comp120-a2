046f089dfed4decd64c4e4468d26af4bbbfc5854
Trevor John <trevorrjohn@gmail.com>
2011-10-15 20:55:35 -0400

    trying to add image but not working

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,17 @@
 function init() {
   var request;
+  var src = "http://maps.googleapis.com/maps/api/js?sensor=true";
   var map;
+  var image = new Image();
+  image.src = "restaurant-71.png"
+  var data;
   var initialLocation;
   var siberia = new google.maps.LatLng(60, 105);
   var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
   var browserSupportFlag =  new Boolean();
   var mark;
+  var lat;
+  var lng;
 
     var myOptions = {
       zoom: 12,
@@ -20,10 +26,11 @@ function init() {
       browserSupportFlag = true;
       navigator.geolocation.getCurrentPosition(function(position) {
         console.log("got location using W3C");
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
         initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
         map.setCenter(initialLocation);
         marker(initialLocation, map);
-        send_to_controller(position.coords.latitude, position.coords.longitude);
 
         }, function() {
         handleNoGeolocation(browserSupportFlag);
@@ -33,6 +40,17 @@ function init() {
     handleNoGeolocation(browserSupportFlag);
     }
 
+      /* place map */
+      map.setCenter(initialLocation);
+      marker(initialLocation, map);
+      // To add the marker to the map, call setMap();
+      mark.setMap(map);
+
+      if(browserSupportFlag == true) {
+        console.log("support flag == true");
+        send_to_controller(lat, lng);
+      }
+
     function handleNoGeolocation(errorFlag) {
       if (errorFlag == true) {
         alert("Geolocation service failed.");
@@ -41,17 +59,13 @@ function init() {
         alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
         initialLocation = siberia;
       }
-
-      /* place map */
-      map.setCenter(initialLocation);
-      marker(initialLocation, map);
-      // To add the marker to the map, call setMap();
-      mark.setMap(map);
     }
 
     function marker(initialLocation, map) {
       mark = new google.maps.Marker({
         position: initialLocation,
+        map: map,
+        icon: image,
         title:"Your location"
       });
     }
@@ -59,19 +73,30 @@ function init() {
     function send_to_controller(lat, lng) {
       console.log("in send to controller");
       $.get("/maps/find_nearest_restaurants", { lat: lat, lng: lng }, function(data){
-        var result = JSON.parse(data);
-        console.log(result);
-            });
+        place_on_map(data);
+      });
     }
-}
-      /*
-      request = new XMLHttpRequest();
-      request.open("GET", "/maps/find_nearest_restaurants", true);
-      request.onreadstatechange = function(){
-        if(request.readystate == 4) {
-          results = request.resoponsetext;
-          console.log(JSON.parse(results));
+
+    function place_on_map(data) {
+      var pos;
+       for(var i = 0; i < data.results.length; i++)
+        {
+          console.log(data.results[i].name);
+          pos = new google.maps.LatLng(data.results[i].geometry.location.lat, data.results[i].geometry.lng);
+         restMark = new google.maps.Marker ({
+           position: pos,
+           icon: image,
+           map: map,
+           title: data.results[i].name
+         });
+         console.log(restMark);
+         restMark.setMap(map);
+
         }
-      }
     }
-  }*/
+
+
+
+
+
+}
