d73cadea6efff07e06540d7bd3b27161287b02c9
Trevor John <trevorrjohn@gmail.com>
2011-10-17 16:55:44 -0400

    got map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,90 +13,78 @@ function init() {
   var lat;
   var lng;
 
-    var myOptions = {
-      zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+  var myOptions = {
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+    initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    console.log("Line 27:" + position.coords.latitude + ", " + position.coords.longitude);
+    map.setCenter(initialLocation);
+    marker(initialLocation, map);
 
-    // Try W3C Geolocation (Preferred)
-    if(navigator.geolocation) {
-      browserSupportFlag = true;
-      navigator.geolocation.getCurrentPosition(function(position) {
-        console.log("got location using W3C");
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
-        initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-        map.setCenter(initialLocation);
-        marker(initialLocation, map);
+  }, function() {
+     handleNoGeolocation(browserSupportFlag);
+  });
+  } else {
+  browserSupportFlag = false;
+  handleNoGeolocation(browserSupportFlag);
+  }
 
-        }, function() {
-        handleNoGeolocation(browserSupportFlag);
-      });
+  if(browserSupportFlag == true) {
+    console.log("Line 40:" + lat + ", " + lng);
+    send_to_controller(lat, lng);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
     } else {
-    browserSupportFlag = false;
-    handleNoGeolocation(browserSupportFlag);
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = siberia;
     }
+  }
 
-      /* place map */
-      map.setCenter(initialLocation);
-      marker(initialLocation, map);
-      // To add the marker to the map, call setMap();
-      mark.setMap(map);
+  function marker(initialLocation, map) {
+    mark = new google.maps.Marker({
+      position: initialLocation,
+      map: map,
+      icon: image,
+      title:"Your location"
+    });
+  }
 
-      if(browserSupportFlag == true) {
-        console.log("support flag == true");
-        send_to_controller(lat, lng);
-      }
+  function send_to_controller(lat, lng) {
+    console.log("in send to controller");
+    console.log(lat + ", " + lng);
+    $.get("/maps/find_nearest_restaurants", { lat: lat, lng: lng }, function(data){
+      console.log(data);
+      place_on_map(data);
+    });
+  }
 
-    function handleNoGeolocation(errorFlag) {
-      if (errorFlag == true) {
-        alert("Geolocation service failed.");
-        initialLocation = newyork;
-      } else {
-        alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-        initialLocation = siberia;
-      }
+  function place_on_map(data) {
+    console.log(data);
+    var pos;
+    console.log(data.results.length);
+    for(var i = 0; i < data.results.length; i++) {
+      console.log(data.results[i].name);
+      pos = new google.maps.LatLng(data.results[i].geometry.location.lat, data.results[i].geometry.lng);
+     restMark = new google.maps.Marker ({
+       position: pos,
+       icon: image,
+       map: map,
+       title: data.results[i].name
+     });
+     console.log(restMark);
+     restMark.setMap(map);
+
     }
-
-    function marker(initialLocation, map) {
-      mark = new google.maps.Marker({
-        position: initialLocation,
-        map: map,
-        icon: image,
-        title:"Your location"
-      });
-    }
-
-    function send_to_controller(lat, lng) {
-      console.log("in send to controller");
-      $.get("/maps/find_nearest_restaurants", { lat: lat, lng: lng }, function(data){
-        place_on_map(data);
-      });
-    }
-
-    function place_on_map(data) {
-      var pos;
-       for(var i = 0; i < data.results.length; i++)
-        {
-          console.log(data.results[i].name);
-          pos = new google.maps.LatLng(data.results[i].geometry.location.lat, data.results[i].geometry.lng);
-         restMark = new google.maps.Marker ({
-           position: pos,
-           icon: image,
-           map: map,
-           title: data.results[i].name
-         });
-         console.log(restMark);
-         restMark.setMap(map);
-
-        }
-    }
-
-
-
-
-
+  }
 }
