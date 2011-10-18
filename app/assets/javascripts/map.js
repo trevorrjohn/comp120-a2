e2549e21ebf4f5ce077dfff9211416d50f0d67f6
Trevor John <trevorrjohn@gmail.com>
2011-10-17 22:05:48 -0400

    new js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,48 +1,55 @@
+var initialLocation;
+var browserSupportFlag =  new Boolean();
+var lat;
+var lng;
+var image = new Image();
+    image.src = "/assets/restaurant-71.png";
+var map;
+
 function init() {
-  var request;
-      src = "http://maps.googleapis.com/maps/api/js?sensor=true";
-  var map;
-  var image = new Image();
-      image.src = "/assets/images/restaurant-71.png"
-  var data;
-  var initialLocation;
+  console.log("init");
+  initialize_map();
+  console.log("Lat:" + lat + " lng: "+ lng);
+
+}
+
+function initialize_map() {
   var siberia = new google.maps.LatLng(60, 105);
   var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-  var browserSupportFlag =  new Boolean();
-  var mark;
-
   var myOptions = {
-    zoom: 12,
+    zoom: 6,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-
+  // Try W3C Geolocation (Preferred)
   if(navigator.geolocation) {
     browserSupportFlag = true;
     navigator.geolocation.getCurrentPosition(function(position) {
-    initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-    console.log("Line 30: initial location " + position.coords.latitude + ", " + position.console.longitude);
-    map.setCenter(position.coords.latitude, position.coords.longitude);
-    console.log(map);
-    marker(initialLocation, map);
-
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
     });
-  }
-   else {
-  browserSupportFlag = false;
-  handleNoGeolocation(browserSupportFlag);
-  }
-  /* place map */
-  map.setCenter(initialLocation);
-  marker(initialLocation, map);
-  // To add the marker to the map, call setMap();
-  mark.setMap(map);
-
-  if(browserSupportFlag == true) {
-  //  console.log("Line49: browser support flag = t" + initialLocation.latitude + ", " + initialLocation.longitude);
-    send_to_controller(position.coords.latitude, position.coords.longitude);
+    // Try Google Gears Geolocation
+  } else if (google.gears) {
+    browserSupportFlag = true;
+    var geo = google.gears.factory.create('beta.geolocation');
+    geo.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeoLocation(browserSupportFlag);
+    });
+    // Browser doesn't support Geolocation
+  } else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
   }
 
   function handleNoGeolocation(errorFlag) {
@@ -53,39 +60,6 @@ function init() {
       alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
       initialLocation = siberia;
     }
-  }
-
-  function marker(initialLocation, map) {
-    mark = new google.maps.Marker({
-      position: initialLocation,
-      map: map,
-      icon: image,
-      title:"Your location"
-    });
-  }
-
-  function send_to_controller(lat, lng) {
-    console.log("Line 73: in send to controller" + lat + ", " + lng);
-    $.get("/maps/find_nearest_restaurants", { lat: lat, lng: lng }, function(data){
-      console.log("Line 75: Data returned: " + data);
-      place_on_map(data);
-    });
-  }
-
-  function place_on_map(data) {
-    console.log("Line 81: in place on map:" +   data);
-    var pos;
-    for(var i = 0; i < data.results.length; i++) {
-      console.log(data.results[i].name);
-      pos = new google.maps.LatLng(data.results[i].geometry.location.lat, data.results[i].geometry.lng);
-     restMark = new google.maps.Marker ({
-       position: pos,
-       icon: image,
-       map: map,
-       title: data.results[i].name
-     });
-     restMark.setMap(map);
-
-    }
+    map.setCenter(initialLocation);
   }
 }
